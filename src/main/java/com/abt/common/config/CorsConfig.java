86543f49893e2902bc47b51be4c0291e4c242926
lsyzzzz <leesse@126.com>
2024-05-17 18:49:26 +0800

    dev

--- src/main/java/com/abt/common/config/CorsConfig.java
+++ src/main/java/com/abt/common/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:1803") // 允许的源
+                .allowedOrigins("http://localhost", "http://7386n59g91.goho.co:20397", "http://192.168.0.181") // 允许的源
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 允许的HTTP方法
                 .allowedHeaders("*") // 允许的请求头
                 .allowCredentials(true) // 允许发送凭证信息（如Cookie）
