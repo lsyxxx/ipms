de7c2f221763ae7da405b594eea3f58fc4ad176f
lsyzzzz <leesse@126.com>
2026-03-27 17:24:05 +0800

    修改排至

--- wxapp/src/main/java/com/abt/wxapp/checkmodule/service/WxCheckModuleService.java
+++ wxapp/src/main/java/com/abt/wxapp/checkmodule/service/WxCheckModuleService.java
@@ -1,4 +1,4 @@
 package com.abt.wxapp.checkmodule.service;
 
-public interface CheckModuleService {
+public interface WxCheckModuleService {
 }
--- wxapp/src/main/java/com/abt/wxapp/checkmodule/service/WxCheckModuleServiceImpl.java
+++ wxapp/src/main/java/com/abt/wxapp/checkmodule/service/WxCheckModuleServiceImpl.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Service;
  */
 @Service
 @Slf4j
-public class CheckModuleServiceImpl implements CheckModuleService {
+public class WxCheckModuleServiceImpl implements WxCheckModuleService {
 }
