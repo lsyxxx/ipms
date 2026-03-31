ec88b35244f7e89e9cd7420f1dd9867c5d1d7901
lsyzzzz <leesse@126.com>
2026-03-31 16:13:22 +0800

    dev

--- common/src/main/java/com/abt/chkmodule/repository/CheckItemRepository.java
+++ common/src/main/java/com/abt/chkmodule/repository/CheckItemRepository.java
@@ -1,12 +1,8 @@
 package com.abt.chkmodule.repository;
 
 import com.abt.chkmodule.entity.CheckItem;
-import com.abt.chkmodule.entity.CheckModule;
-import com.abt.common.repository.EnabledRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface CheckItemRepository extends JpaRepository<CheckItem, String>, EnabledRepository<CheckModule, String> {
+public interface CheckItemRepository extends JpaRepository<CheckItem, String> {
 
 }
